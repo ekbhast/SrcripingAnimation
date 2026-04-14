559a09a6cdccd435c3cc4a9306615c2960863927
Alexey Kirillov <ekbhast@yandex.ru>
2026-04-14 09:35:00 +0500

    added last animation, and fix another

--- Assets/Scripts/SphereMover.cs
+++ Assets/Scripts/SphereMover.cs
@@ -1,15 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SphereMover : MonoBehaviour
 {
     [SerializeField] private Transform _sphere;
     [SerializeField] private float _speed;
-    [SerializeField] private Vector3 _direction;
 
     private void Update()
     {
-        _sphere.transform.Translate(_direction * _speed * Time.deltaTime);
+        _sphere.transform.Translate(Vector3.forward * _speed * Time.deltaTime);
     }
 }
